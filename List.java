9e278236f16ba87d97e540f4e66066074eadbfff
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-08 14:21:22 +0530

    Update List.java

--- List.java
+++ List.java
@@ -1,27 +1,34 @@
+package in.pacasian.sumithkumar.q;
 
-package com.example.Pacasian.pacy;
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * Created by sumith on 16-10-2017.
- */
-
-public class about extends AppCompatActivity {
-
-private Button button;
+public class List extends AppCompatActivity {
+    ListView listV;
+    String[] name = {"Materials", "Create a Project", "Visit Site", "Profile", "About Us !", "Contact Us"};
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
-        button=(TextView)findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.list);
+        listV=(ListView)findViewById(R.id.list_for_beg);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,name);
+        listV.setAdapter(adapter);
+        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-               //Add your operation here.......
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SimpleCamera.this, "Item "+position, Toast.LENGTH_SHORT).show();
             }
         });
     }
